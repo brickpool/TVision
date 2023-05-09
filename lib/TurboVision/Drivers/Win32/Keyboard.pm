fe62951ed6b66d5c5f2cc3b74968fd1ce478fe6b
J. Schneider <36104448+brickpool@users.noreply.github.com>
2023-05-09 20:12:46 +0200

    Move Utility Routines to Utility.pm

--- lib/TurboVision/Drivers/Win32/Keyboard.pm
+++ lib/TurboVision/Drivers/Win32/Keyboard.pm
@@ -2,7 +2,7 @@
 
 =head1 NAME
 
-TurboVision::Drivers::Win32::Keyboard - Windows Keyboard Manager
+TurboVision::Drivers::Win32::Keyboard - Event Manager Keyboard implementation
 
 =cut
 
@@ -48,7 +48,20 @@ use TurboVision::Drivers::Types qw(
   TEvent
   is_TEvent
 );
-use TurboVision::Drivers::Win32::EventManager qw( :private );
+require TurboVision::Drivers::Win32::EventManager;
+
+# ------------------------------------------------------------------------
+# Imports ----------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+alias our @_event_queue
+  = @TurboVision::Drivers::Win32::EventManager::_event_queue;
+
+alias our $_shift_state
+  = $TurboVision::Drivers::Win32::EventManager::_shift_state;
+
+*_update_event_queue
+  = \&TurboVision::Drivers::Win32::EventManager::_update_event_queue;
 
 # ------------------------------------------------------------------------
 # Exports ----------------------------------------------------------------
@@ -61,11 +74,6 @@ Nothing per default, but can export the following per request:
   :all
   
     :kbd
-      ctrl_to_arrow
-      get_alt_char
-      get_alt_code
-      get_ctrl_char
-      get_ctrl_code
       get_key_event
       get_shift_state
 
@@ -79,11 +87,6 @@ our @EXPORT_OK = qw(
 our %EXPORT_TAGS = (
 
   kbd => [qw(
-    ctrl_to_arrow
-    get_alt_char
-    get_alt_code
-    get_ctrl_char
-    get_ctrl_code
     get_key_event
     get_shift_state
   )],
@@ -110,123 +113,6 @@ our %EXPORT_TAGS = (
 =head2 Subroutines
 
 =over
-
-=item public C<< Int ctrl_to_arrow(Int|Str $key_code) >>
-
-This esoteric function converts certain control-key combinations to standard
-I<kbXXXX> constant values.
-
-The original WordStar word processor used a standard set of Ctrl-letter
-combinations as equivalents to IBM cursor movement keys and set a standard
-interpretation of these keys when used in word processing.
-
-To assist in providing WordStar compatibility, I<ctrl_to_arrow> can be used to
-convert I<< $event->key_code >> values to their corresponding I<kbXXXX> values.
-
-The following chart shows the mapping from control keys to I<kbXXXX> values.
-
-  Control key   Lo(Keycode)   Maps to
-  Ctrl-A        0x01          KB_HOME
-  Ctrl-D        0x04          KB_RIGHT
-  Ctrl-E        0x05          KB_UP
-  Ctrl-F        0x06          KB_END
-  Ctrl-G        0x07          KB_DEL
-  Ctrl-S        0x13          KB_LEFT
-  Ctrl-V        0x16          KB_INS
-  Ctrl-X        0x18          KB_DOWN
-
-=cut
-
-  func ctrl_to_arrow(Int|Str $key_code) {
-    return _WORD_STAR_CODES->( $key_code ) // $key_code;
-  }
-
-=item public C<< Str get_alt_char(Int $key_code) >>
-
-When I<$key_code> specifies an I<Alt+Ch> character combination, where I<Ch> is a
-letter from 'A' to 'Z', I<get_alt_char> extracts and returns the I<Ch>
-character value.
-
-For example,
-  
-  print get_alt_char( KB_ALT_A );
-
-prints the single letter I<A>.
-
-Method I<get_alt_code> maps characters back to I<Alt+Ch> combinations.
-
-=cut
-
-  func get_alt_char(Int $key_code) {
-    return chr(0xf0)                                    # special case Alt-Space
-        if $key_code == 0x0200;
-
-    return "\0"                                         # no extended key
-        if $key_code & 0xff;
-
-    my $hi = $key_code >> 8 & 0xff;
-
-    if ( $hi <= 0x83 ) {                                # highest value in list
-      for my $i (0..127) {                              # search for match
-        return chr($i)                                  # return character
-            if $hi == _ALT_CODES->( $i );
-      }
-    }
-
-    return "\0";
-  }
-
-=item public C<< Int get_alt_code(Str $ch) >>
-
-Maps a single character 'A' to 'Z' to the Keycode equivalent value for pressing
-I<Alt+Ch>.
-
-For example,
-
-  get_alt_code(Str $ch);
-
-returns C<0x1e00>, which is the value of KB_ALT_A.
-
-The subroutine I<get_alt_code> is the inverse function to I<get_alt_char>.
-
-=cut
-
-  func get_alt_code(Str $ch) {
-    return 0x0200
-        if ord($ch) == 0xf0;                            # special case Alt-Space
-      
-    $ch = ord( uc($ch) );
-    
-    return _ALT_CODES->($ch) << 8
-        if $ch >= 0 && $ch <= 127;
-
-    return 0;
-  }
-
-=item public C<< Str get_ctrl_char(Int $key_code) >>
-
-Returns the ASCII character for the I<Ctrl+Key> scancode that was given.
-
-=cut
-
-  func get_ctrl_char(Int $key_code) {
-    my $lo = $key_code & 0xff;
-
-    return chr($lo + 0x40)                              # return char a-z
-        if $lo >= 1 && $lo <= 26;                       # between 1-26
-
-    return "\0";
-  }
-
-=item public C<< Int get_ctrl_code(Str $ch) >>
-
-Returns the scancode corresponding to I<Ctrl+Ch> key that is given.
-
-=cut
-
-  func get_ctrl_code(Str $ch) {
-    return get_alt_code($ch) | ord($ch) - 0x40;         # Ctrl+key code
-  }
 
 =item public C<< get_key_event(TEvent $event) >>
 
@@ -271,7 +157,7 @@ value contains a combination of the I<kbXXXX> constants for shift states.
 =cut
 
   func get_shift_state() {
-    return $TurboVision::Drivers::Win32::EventManager::_shift_state;
+    return $_shift_state;
   }
 
 =back
