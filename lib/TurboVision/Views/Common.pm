f717df1d91e98ba3cdadf10b28e5b1807c38d2d2
J. Schneider <36104448+brickpool@users.noreply.github.com>
2023-05-20 21:47:47 +0200

    Create Common.pm

---
+++ lib/TurboVision/Views/Common.pm
@@ -0,0 +1,197 @@
+=pod
+
+=head1 NAME
+
+TurboVision::Views::Common - Variables and tools used by I<Views>
+
+=cut
+
+package TurboVision::Views::Common;
+
+# ------------------------------------------------------------------------
+# Boilerplate ------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+use 5.014;
+use warnings;
+
+use Function::Parameters {
+  func => {
+    defaults    => 'function_strict',
+    name        => 'required',
+  },
+};
+
+use MooseX::Types::Moose qw( :all );
+
+# version '...'
+our $VERSION = '2.000_001';
+$VERSION =~ tr/_//d;
+
+# authority '...'
+our $AUTHORITY = 'github:fpc';
+
+# ------------------------------------------------------------------------
+# Used Modules -----------------------------------------------------------
+# ------------------------------------------------------------------------
+
+use TurboVision::Const qw( :bool );
+use TurboVision::Views::Const qw( :cmXXXX );
+
+# ------------------------------------------------------------------------
+# Exports ----------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+=head1 EXPORTS
+
+Nothing per default, but can export the following per request:
+
+  :all
+
+    :vars
+      $command_set_changed
+      $cur_command_set
+  
+=cut
+
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(
+);
+
+our %EXPORT_TAGS = (
+
+  vars => [qw(
+    $command_set_changed
+    $cur_command_set
+  )],
+
+);
+
+# add all the other %EXPORT_TAGS ":class" tags to the ":all" class and
+# @EXPORT_OK, deleting duplicates
+{
+  my %seen;
+  push
+    @EXPORT_OK,
+      grep {!$seen{$_}++} @{$EXPORT_TAGS{$_}}
+        foreach keys %EXPORT_TAGS;
+  push
+    @{$EXPORT_TAGS{all}},
+      @EXPORT_OK;
+}
+
+# ------------------------------------------------------------------------
+# Variables --------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+=head2 Variables
+
+=over
+
+=item public C<< Bool $command_set_changed >>
+
+True if the command set has changed since being set to false.
+
+=cut
+
+  our $command_set_changed = _FALSE;
+
+=item public C<< Str $cur_command_set >>
+
+I<$cur_command_set> is a string created with Perl's command I<vec>.
+
+It stores the current command set. By default, all commands except those of the
+window are active.
+
+=end comment
+
+=cut
+
+  our $cur_command_set = pack( 'b*', 1 x 256 );
+  vec ($cur_command_set, $_, 1) = 0                       # All active but these
+    foreach CM_ZOOM, CM_CLOSE, CM_RESIZE, CM_NEXT, CM_PREV;
+
+=back
+
+=cut
+
+# ------------------------------------------------------------------------
+# Subroutines ------------------------------------------------------------
+# ------------------------------------------------------------------------
+
+=head2 Subroutines
+
+=over
+
+=item public C<< get_mouse_event(TEvent $event) >>
+
+...
+
+=cut
+
+  func get_mouse_event($) {
+  }
+
+=back
+
+=cut
+
+1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENCE
+
+ This file is part of the port of the Free Vision library.
+ Copyright (c) 1996-2000 by Leon de Boer.
+
+ Interface Copyright (c) 1992 Borland International
+
+ The library files are licensed under modified LGPL.
+
+ The creation of subclasses of this LGPL-licensed class is considered to be
+ using an interface of a library, in analogy to a function call of a library.
+ It is not considered a modification of the original class. Therefore,
+ subclasses created in this way are not subject to the obligations that an LGPL
+ imposes on licensees.
+
+ POD sections by Ed Mitchell are licensed under modified CC BY-NC-ND.
+
+=head1 AUTHORS
+ 
+=over
+
+=item *
+
+1996-1999 by Leon de Boer E<lt>ldeboer@attglobal.netE<gt>
+
+=item *
+
+1992 by Ed Mitchell (Turbo Pascal Reference electronic freeware book)
+
+=back
+
+=head1 DISCLAIMER OF WARRANTIES
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+
+=head1 MAINTAINER
+
+=over
+
+=item *
+
+2023 by J. Schneider L<https://github.com/brickpool/>
+
+=back
+
+=head1 SEE ALSO
+
+L<views.pas|https://github.com/fpc/FPCSource/blob/bdc826cc18a03a833735853c0c91268c992e8592/packages/fv/src/views.pas>
