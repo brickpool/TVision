63a65d9cf0e351eeb2b3a02ee5dbb3e1507375db
brickpool <36104448+brickpool@users.noreply.github.com>
2024-12-03 20:54:16 +0100

    Update MANIFEST and Makefile

--- lib/TV/Objects/Object.pm
+++ lib/TV/Objects/Object.pm
@@ -11,6 +11,10 @@ package TV::Objects::Object;
 use strict;
 use warnings;
 
+our $VERSION = '2.000_001';
+$VERSION =~ tr/_//d;
+our $AUTHORITY = 'cpan:BRICKPOOL';
+
 use Exporter 'import';
 our @EXPORT = qw(
   TObject
